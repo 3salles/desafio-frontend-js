0085fbdf3f7af601fd73fe6c003d7c4902bc7525
Beatriz <beatrizsallesss@gmail.com>
2021-03-04 10:17:13 -0300

    ➕ chore: Add Eslint dependency and setup.

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>MOBRJ</title>
+      </Head>
+
+    <h1>MOBRJ</h1>
+    </div>
+  )
+}
