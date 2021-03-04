db27c164203517bcf08b48a82a93350e78ec9df8
Beatriz <beatrizsallesss@gmail.com>
2021-03-04 10:54:28 -0300

    🔧 chore: Create tailwind config.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import Head from 'next/head'
+import resolveConfig from 'tailwindcss/resolveConfig'
+import tailwindConfig from '../../tailwind.config'
 
+const fullConfig = resolveConfig(tailwindConfig)
 
 export default function Home() {
   return (
