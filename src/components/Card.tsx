7b5758ce9e579d74fecca7d697d1e41e6959b04c
Beatriz <beatrizsallesss@gmail.com>
2021-03-08 19:34:27 -0300

    ♻️ refactor: Move Recommended content to its own JSON file.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,17 +5,18 @@ import resolveConfig from 'tailwindcss/resolveConfig'
 import tailwindConfig from '../../tailwind.config'
 import Icon from "./Icon";
 
+interface CardContent {
+  image: string
+  tag: "interno" | "confidencial"
+  icon_name: string
+  metric: string
+  title: string
+  category: string
+  averageTime?: number
+  percentage?: number
+}
 interface CardProps {
-  cardContent: {
-    image: string
-    tag: "interno" | "confidencial"
-    icon: FC
-    metric: string
-    title: string
-    category: string
-    averageTime?: number
-    percentage?: number
-  }
+  cardContent: CardContent[]
   className?: string
 }
 
@@ -25,33 +26,35 @@ export default function Card ({ cardContent, className}: CardProps) {
   const tagClass = (cardContent.tag === "confidencial" ? "text-orange-500" : "text-success-500")
 
   return (
-    <div className={className}>
+    <>
+    {cardContent.map((content) => (
+      <div className={className}>
       <Link href="/post">
         <a className="relative w-full flex flex-col">
           <span className={'xs:hidden opacity-80 rounded-br rounded-tl absolute top-0 left-0 z-10 px-4 bg-base-white uppercase text-xs tracking-wider ' + tagClass}>
-            {cardContent.tag}
+            {content.tag}
           </span>
           <Image
           className="rounded"
           layout="responsive"
-          src={cardContent.image}
+          src={content.image}
           width="336"
           height="195"
           />
           <div className="flex rounded-full bg-base-white opacity-80 absolute bottom-4 right-4 px-2 py-0.5 items-center">
-            <cardContent.icon />
+            <Icon size={14} name={content.icon_name} color={defaultConfig.theme.colors.action[600]}/>
             <div className="border-l border-action-600 mx-2 h-3"/>
             <span className="text-action-600 text-xs">
-              {cardContent.metric}
+              {content.metric}
             </span>
           </div>
 
-          {cardContent.percentage && (
+          {content.percentage && (
             <div className="absolute bottom-0 left-0 w-full h-1">
               <div className="bg-action-600 w-full h-full rounded-b">
                 <div
-                className={'bg-vibrant-500 h-full ' + (cardContent.percentage === 100 ? 'rounded-b' : 'rounded-bl') }
-                style={{ width: `${cardContent.percentage}%` }}
+                className={'bg-vibrant-500 h-full ' + (content.percentage === 100 ? 'rounded-b' : 'rounded-bl') }
+                style={{ width: `${content.percentage}%` }}
                 />
               </div>
             </div>
@@ -59,12 +62,12 @@ export default function Card ({ cardContent, className}: CardProps) {
         </a>
       </Link>
       <p className="text-sm text-grey-800 mt-4 mb-1 truncate">
-        {cardContent.title}
+        {content.title}
       </p>
       <div className="flex items-center text-grey-500 text-xs">
-        <span>{cardContent.category}</span>
+        <span>{content.category}</span>
 
-        {cardContent.averageTime && (
+        {content.averageTime && (
           <>
           <div className="border-l border-grey-500 mx-2 h-4"/>
           <Icon
@@ -72,10 +75,13 @@ export default function Card ({ cardContent, className}: CardProps) {
           color={defaultConfig.theme.colors.grey[500]}
           name="clock"
           />
-          <span className="ml-1">+ {cardContent.averageTime} min</span>
+          <span className="ml-1">+ {content.averageTime} min</span>
           </>
         )}
       </div>
     </div>
+    ))}
+</>
+
   )
 }
