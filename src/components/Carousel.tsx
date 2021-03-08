77abf3209b93c931b0a264ce7567a1f32bd4f26b
Beatriz <beatrizsallesss@gmail.com>
2021-03-08 11:09:16 -0300

    ✨ feat: Create Carousel Component.

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,111 @@
+import { Children, FC, ReactNode, useState } from 'react'
+import { TOptions } from 'keen-slider'
+import { useKeenSlider } from 'keen-slider/react'
+import Icon from './Icon'
+
+import "keen-slider/keen-slider.min.css"
+
+
+interface ArrowsType {
+  show: boolean
+  nextArrow?: FC
+  prevArrow?: FC
+}
+
+interface DotsType {
+  show: boolean
+}
+
+interface CarouselProps {
+  children: ReactNode
+  dots?: DotsType
+  arrows?: ArrowsType
+  options?: TOptions
+  slideHeightClass?: string
+}
+
+const defaultDots: DotsType = {
+  show: false
+}
+
+const defaultArrows: ArrowsType = {
+  show: false,
+  nextArrow: () => <Icon name="arrow-right-slim" />,
+  prevArrow: () => <Icon name="arrow-left-slim" />
+}
+
+export default function Carousel ({
+  children,
+  dots = defaultDots,
+  arrows = defaultArrows,
+  slideHeightClass = '',
+  options
+}: CarouselProps
+) {
+  const { nextArrow: NextArrow, prevArrow: PrevArrow } = { ...defaultArrows, ...arrows }
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const [loaded, setLoaded] = useState(false)
+  const [sliderRef, slider] = useKeenSlider<HTMLDivElement>({
+    mounted (s) {
+      setLoaded(true)
+    },
+    slideChanged (s) {
+      if (dots.show) {
+        setCurrentSlide(s.details().relativeSlide)
+      }
+    },
+    ...options
+  })
+
+  const childrenLength = Children.toArray(children).length
+  const slideLenght = currentSlide + 1
+
+  return (
+    <div className="relative w-full">
+      <div ref={sliderRef} className={'keen-slider ' + slideHeightClass}>
+        {Children.map(children, (child, index) => (
+          <div
+            key={index}
+            className="keen-slider__slide"
+            style={{ width: `calc(100% / ${options.slidesPerView as number || 1} - ${options.spacing / 2})` }}
+          >
+            {loaded && child}
+          </div>
+        ))}
+      </div>
+
+      {slider && arrows.show && (
+        <>
+          {currentSlide > 0 && (
+            <button
+              className="hidden lg:block absolute top-2/4 xl:transform xl:-translate-y-2/4 left-5 p-1 focus:outline-none"
+              onClick={() => slider.prev()}
+            >
+              <PrevArrow />
+            </button>
+          )}
+          {slideLenght < childrenLength && (
+            <button
+              className="hidden lg:block absolute top-2/4 xl:transform xl:-translate-y-2/4 right-5 p-1 focus:outline-none"
+              onClick={() => slider.next()}
+            >
+              <NextArrow />
+            </button>
+          )}
+        </>
+      )}
+
+      {slider && dots.show && (
+        <div className="flex absolute space-x-4 dots">
+          {Array.from(Array(slider?.details().size).keys()).map(i => (
+            <button
+              key={i}
+              onClick={() => slider.moveToSlideRelative(i)}
+              className={'focus:outline-none rounded-full w-2.5 h-2.5' + (currentSlide === i ? ' bg-base-white' : ' border-2 border-base-white')}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import Head from 'next/head'
+import Carousel from '../components/Carousel'
 import Header from '../components/Header'
 import Navbar from '../components/Navbar'
 import navbarLinks from '../utils/links.json'
 
 
-export default function Home() {
+
+export default function Home () {
   return (
     <>
       <Head>
@@ -12,12 +14,78 @@ export default function Home() {
       </Head>
 
       <div className="3xl:container mx-auto pb-16 lg:pb-0 lg:flex">
-        <Navbar links ={navbarLinks} />
-
+        <Navbar links={navbarLinks}/>
         <div className="flex-1">
           <div className="relative">
-            <Header className="absolute z-10 px-5 sm:px-8 xl:px-16 items-center"/>
+            {/* <Header className="absolute z-10 px-5 sm:px-8 xl:px-16 items-center"/> */}
+            <Carousel
+              slideHeightClass="h-74 lg:h-84"
+              arrows={{
+                show: true
+              }}
+              dots={{
+                show: true
+              }}
+              options={{
+                rubberband: false,
+                loop: false
+              }}
+            >
+              <div className="h-74 lg:h-84 bg-center bg-no-repeat bg-cover" style={{ backgroundImage: 'url("./assets/carousel-image.png")' }}>
+                <div className="flex flex-col h-full w-full text-base-white px-5 sm:px-8 xl:px-16 py-8 xl:pb-12 pt-20 xl:pt-28">
+                  <h1 className="text-2xl mb-2 tracking-wider lg:text-2xl md:w-6/12 lg:w-full xl:w-5/12">
+                    <span className="text-vibrant-500">Lorem</span> ipsum dolor sit amet,
+                    consectetuer adipiscing.
+                  </h1>
+                  <p className="text-sm leading-6 line-clamp-1 xs:line-clamp-2 md:w-5/12">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu suspendisse vel sed vulputate et leo tincidunt.
+                  </p>
+
+                  <button
+                    className="ml-auto mt-auto w-max py-3 px-6 xl:px-7 bg-gradient-to-r from-action-500 to-action-600 text-xs md:text-sm rounded tracking-widest"
+                  >
+                    VER MAIS
+                  </button>
+                </div>
+              </div>
+              <div className="h-74 lg:h-84 bg-center bg-no-repeat bg-cover" style={{ backgroundImage: 'url("./assets/carousel-image.png")' }}>
+                <div className="flex flex-col h-full w-full text-base-white px-5 sm:px-8 xl:px-16 py-8 xl:pb-12 pt-20 xl:pt-28">
+                  <h1 className="text-2xl mb-2 tracking-wider lg:text-2xl md:w-6/12 lg:w-full xl:w-5/12">
+                    <span className="text-vibrant-500">Lorem</span> ipsum dolor sit amet,
+                    consectetuer adipiscing.
+                  </h1>
+                  <p className="text-sm leading-6 line-clamp-1 xs:line-clamp-2 md:w-5/12">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu suspendisse vel sed vulputate et leo tincidunt.
+                  </p>
+
+                  <button
+                    className="ml-auto mt-auto w-max py-3 px-6 xl:px-7 bg-gradient-to-r from-action-500 to-action-600 text-xs md:text-sm rounded tracking-widest"
+                  >
+                    VER MAIS
+                  </button>
+                </div>
+              </div>
+              <div className="h-74 lg:h-84 bg-center bg-no-repeat bg-cover" style={{ backgroundImage: 'url("./assets/carousel-image.png")' }}>
+                <div className="flex flex-col h-full w-full text-base-white px-5 sm:px-8 xl:px-16 py-8 xl:pb-12 pt-20 xl:pt-28">
+                  <h1 className="text-2xl mb-2 tracking-wider lg:text-2xl md:w-6/12 lg:w-full xl:w-5/12">
+                    <span className="text-vibrant-500">Lorem</span> ipsum dolor sit amet,
+                    consectetuer adipiscing.
+                  </h1>
+                  <p className="text-sm leading-6 line-clamp-1 xs:line-clamp-2 md:w-5/12">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu suspendisse vel sed vulputate et leo tincidunt.
+                  </p>
+
+                  <button
+                    className="ml-auto mt-auto w-max py-3 px-6 xl:px-7 bg-gradient-to-r from-action-500 to-action-600 text-xs md:text-sm rounded tracking-widest"
+                  >
+                    VER MAIS
+                  </button>
+                </div>
+              </div>
+            </Carousel>
           </div>
+
+
         </div>
       </div>
     </>
